d50d1e3efc0af763c7cf24beaff5f2226ed960ca
Healaa344 <2784507501@qq.com>
2026-09-10 17:17:46 +0800

    Add files via upload

---
+++ app/src/main/java/com/mingsheng/mbti/MainActivity.kt
@@ -0,0 +1,11 @@
+
+package com.mingsheng.mbti
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity: Activity(){
+ override fun onCreate(savedInstanceState: Bundle?){
+  super.onCreate(savedInstanceState)
+ }
+}
